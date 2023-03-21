31665f5c63c24de11527f13dea20a8fe2057fa9f
LIARSSS <arturs.apinis1@gmail.com>
2023-03-21 20:12:58 +0200

    2.1 istais

--- build_heap.py
+++ build_heap.py
@@ -3,19 +3,19 @@ import math
 
 def build_heap(data):
     swaps = []
-    # n = len(data)
-    # for i in reversed(range(math.ceil(n / 2))):
-    #     n = len(data)
-    #     i_max = i
-    #     val = 2 * i + 1
-    #     if val < n and data[val] < data[i_max]:
-    #         i_max = val
-    #     r = 2 * i + 2
-    #     if r < n and data[r] < data[i_max]:
-    #         i_max = r
-    #     if i != i_max:
-    #         swaps.append((i, i_max))
-    #         data[i], data[i_max] = data[i_max], data[i]
+    n = len(data)
+    for i in reversed(range(math.ceil(n / 2))):
+        n = len(data)
+        i_max = i
+        val = 2 * i + 1
+        if val < n and data[val] < data[i_max]:
+            i_max = val
+        r = 2 * i + 2
+        if r < n and data[r] < data[i_max]:
+            i_max = r
+        if i != i_max:
+            swaps.append((i, i_max))
+            data[i], data[i_max] = data[i_max], data[i]
     return swaps
 
 
