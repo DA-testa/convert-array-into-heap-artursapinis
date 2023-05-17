f6669b11f741deca37987223dd290f9ff229d470
LIARSSS <arturs.apinis1@gmail.com>
2023-05-17 16:41:55 +0300

    lab 444

--- build_heap.py
+++ build_heap.py
@@ -1,11 +1,9 @@
 import math
 
-
 def build_heap(data):
     swaps = []
     n = len(data)
     for i in reversed(range(math.ceil(n / 2))):
-        n = len(data)
         i_max = i
         val = 2 * i + 1
         if val < n and data[val] < data[i_max]:
@@ -16,8 +14,8 @@ def build_heap(data):
         if i != i_max:
             swaps.append((i, i_max))
             data[i], data[i_max] = data[i_max], data[i]
+            swaps.extend(build_heap(data[i_max:]))
     return swaps
-
 
 def main():
     n = int(input())
@@ -30,7 +28,6 @@ def main():
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
